553e52fe57d60e84e40898e8a989913ae5751399
Julio Collazos <amarullazo626@gmail.com>
2025-11-24 16:09:52 -0500

    fix: got rid of unused functions

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,3 @@
 is_name_safe <- function(name) {
   make.names(name) == name
 }
-test_first_letter <- function(char) {
-  regex <- "[[:alpha:].]"
-  grepl(pattern = regex, char, perl = TRUE)
-}
-test_next_letters <- function(char) {
-  regex <- "[[:alpha:]]"
-  grepl(pattern = regex, char, perl = TRUE)
-}
