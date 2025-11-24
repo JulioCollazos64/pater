993a4b2a9ee02e2dbee35633961b2b1908acfa3f
Julio Collazos <amarullazo626@gmail.com>
2025-11-24 17:37:04 -0500

    Added a new utility function

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,18 @@
 isNameSafe <- function(name) {
   make.names(name) == name
 }
+
+#' Validate next token
+#'
+#' @param token token following a parameter token
+#'
+#' @noRd
+#' @keywords internal
+isNextNameSafe <- function(token) {
+  regex_next <- "[a-zA-Z0-9\\p{L}._]"
+  if (token$type == "text") {
+    letter <- substr(token$value, 1, 1)
+    return(!grepl(regex_next, letter, perl = TRUE))
+  }
+  TRUE
+}
