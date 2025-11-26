5a0f46f6ad089689012d52b72f990ec7088d206f
Julio Collazos <amarullazo626@gmail.com>
2025-11-25 20:03:51 -0500

    ch: move util functions related to stringify to the stringufy file instead

--- R/utils.R
+++ R/utils.R
@@ -1,33 +1,3 @@
-#' Validate name
-#'
-#' @noRd
-#' @keywords internal
-#' @param name
-isNameSafe <- function(name) {
-  make.names(name) == name
-}
-
-#' Validate next token
-#'
-#' @param token An object of class `tokenData`.
-#'
-#' @noRd
-#' @keywords internal
-isNextNameSafe <- function(token) {
-  regex_next <- regexOthers()
-  token <- token[[1]]
-
-  if (is.null(token)) {
-    return(TRUE)
-  }
-
-  if (token$type == "text") {
-    letter <- substr(token$value, 1, 1)
-    return(!grepl(regex_next, letter, perl = TRUE))
-  }
-  TRUE
-}
-
 #' Valid first character in R
 #'
 #' @noRd
