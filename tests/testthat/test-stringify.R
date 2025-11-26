20dbf788cffb7d16cecb95f6cdd3cd8b3b73e4ac
Julio Collazos <amarullazo626@gmail.com>
2025-11-25 20:39:43 -0500

    test: test for stringify

---
+++ tests/testthat/test-stringify.R
@@ -0,0 +1,67 @@
+test_that("Stringify works", {
+  tokens <- buildTokenData(
+    list(
+      list(
+        type = "text",
+        value = "/users/"
+      ),
+      list(
+        type = "param",
+        name = "userId"
+      ),
+      list(
+        type = "text",
+        value = "/posts/"
+      ),
+      list(
+        type = "param",
+        name = "postId"
+      )
+    )
+  )
+  path <- stringifyTokens(tokens)
+
+  expect_identical(
+    path,
+    "/users/:userId/posts/:postId"
+  )
+
+  tokens <- buildTokenData(
+    list(
+      list(
+        type = "text",
+        value = "/users/"
+      ),
+      list(
+        type = "param",
+        name = "id"
+      ),
+      list(
+        type = "text",
+        value = "randomText"
+      )
+    )
+  )
+  path <- stringifyTokens(tokens)
+
+  expect_identical(
+    path,
+    '/users/:"id"randomText'
+  )
+})
+
+test_that("Stringify errors", {
+  tokens <- buildTokenData(
+    list(
+      list(
+        type = "non-existent",
+        value = "/users/"
+      )
+    )
+  )
+
+  expect_error(
+    stringifyTokens(tokens),
+    regexp = "Unknown .* type"
+  )
+})
